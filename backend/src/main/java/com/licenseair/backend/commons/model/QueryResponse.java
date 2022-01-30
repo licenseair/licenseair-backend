c50b4ac2113a6af514280b64fadc7b27daac17de
foxsir <838394225@qq.com>
2022-01-30 21:30:30 +0800

    增加扩展信息（extra）

--- backend/src/main/java/com/licenseair/backend/commons/model/QueryResponse.java
+++ backend/src/main/java/com/licenseair/backend/commons/model/QueryResponse.java
@@ -6,8 +6,24 @@ import org.springframework.http.HttpStatus;
 import java.util.Map;
 
 public class QueryResponse {
+  /**
+   * 是否存在错误 =0:不存在
+   */
   public final int error = 0;
+
+  /**
+   * 错误号
+   */
   public final int code = HttpStatus.OK.value();
+
+  /**
+   * 扩展信息
+   */
+  public String extra = null;
+
+  /**
+   * 返回数据
+   */
   public final Object data;
 
   public QueryResponse(DataResource object) {
